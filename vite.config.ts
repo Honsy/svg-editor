eeb580151cb44a20b7381fa36cc47c26e2aa9665
Honsy <hongshaoy@outlook.com>
2023-04-22 19:31:50 +0800

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
+  base:'./svg-editor',
   resolve:{
     alias:{
       '@': path.resolve(__dirname, "src"),
